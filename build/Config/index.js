4b8f906ba94443e59fb19e4da31048b558e3e02a
Tim Osipchik <tsimafei.osipchyk@interexy.com>
2023-02-24 02:28:30 +0300

    script fixes

--- build/Config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const serverConfig = {};
-const routerConfig = {};
-function Config() {
-}
-exports.default = Config;
-module.exports = Config;
-//# sourceMappingURL=index.js.map
