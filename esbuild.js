f89f907834da063e7b8efbfbbe8a97e1c6a0a8e8
Tim-Tapline <tsimafei@taplane.com>
2023-09-10 23:52:10 +0300

    fix

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,15 @@
-import fs from 'fs';
+import { file } from 'bun';
 
-const packageBase = JSON.parse(fs.readFileSync('./package.json'));
+const packageBase = JSON.parse(file('./package.json'));
+const readMe = JSON.parse(file('./README.md'));
 
-const version = packageBase.version.split('.');
-version[2] = Number(version[2]) + 1;
-
-packageBase.version = version.join('.');
+// const version = packageBase.version.split('.');
+// version[2] = Number(version[2]) + 1;
+//
+// packageBase.version = version.join('.');
 
 const { scripts, devDependencies, type, ...common } = packageBase;
 const packageBuild = common;
 
-fs.writeFileSync('./build/package.json', JSON.stringify(packageBuild, null, 2));
-fs.writeFileSync('./package.json', JSON.stringify(packageBase, null, 2));
+await Bun.write('./build/README.md', readMe);
+await Bun.write('./build/package.json', JSON.stringify(packageBuild, null, 2));
